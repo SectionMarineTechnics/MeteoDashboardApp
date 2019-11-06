e2e86144f922bba19de3427d35437ca0b49462eb
BartDecloedt <bart.decloedt@gmail.com>
2019-11-06 11:55:25 +0100

    Add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: ' ', component: DashboardComponent/*, canActivate: [AuthGuard]*/ },
+  { path: 'Dashboard', component: DashboardComponent/*, canActivate: [AuthGuard]*/ },
+  { path: 'Weather', component: WeatherComponent/*, canActivate: [AuthGuard]*/ },
+  { path: 'login', component: WeatherComponent },
+  { path: 'register', component: WeatherComponent },
+  { path: 'profile', component: WeatherComponent/*, canActivate: [AuthGuard]*/ },
+
+  // otherwise redirect to Dashboard
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatMenuModule } from '@angular/material/menu'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WeatherComponent } from './components/weather/weather.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
